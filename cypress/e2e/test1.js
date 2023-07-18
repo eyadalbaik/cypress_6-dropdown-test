6aeac66842fd868b1a01da45a4f0aab52795d921
eyadalbaik <eyadalbaik01@gmail.com>
2023-07-19 02:57:39 +0300

    test

---
+++ cypress/e2e/test1.js
@@ -0,0 +1,15 @@
+describe("log in" , ()=>{
+  it ("test" , ()=>{
+
+      cy.visit("https://codenboxautomationlab.com/practice/");
+      
+
+
+// cy.get("#dropdown-class-example").select("API")
+
+
+cy.get("#dropdown-class-example").select("Selenium")
+
+
+  })
+})
